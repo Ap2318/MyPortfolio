31c0befab5d87f2c1ad59075a551091af4fae14a
Arpit Pareshbhai Patel <apatel4@isimtl.local>
2021-04-15 21:13:22 -0400

    Added MyPortfolio

---
+++ MyPortfolio/projects.php
@@ -0,0 +1,270 @@
+<html>
+
+<head>
+	<title>Projects</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<link rel="icon" type="image/png" href="images/logoP.png">
+	<link rel="stylesheet" href="assets/css/main.css" />
+</head>
+
+<body class="is-preload">
+
+	<!-- Header -->
+	<header id="header">
+		<a class='logo'>Arpit Patel</a>
+		<nav>
+			<a href="#menu">Menu</a>
+		</nav>
+	</header>
+
+	<!-- Nav -->
+	<nav id="menu">
+		<ul class="links">
+			<li><a href="index.php#banner">Home</a></li>
+			<li><a href="index.php#projects">Projects</a></li>
+			<li><a href="index.php#technicalskills">Technical Skills</a></li>
+			<li><a href="index.php#contactme">Contact Me</a></li>
+			<li><a href="index.php#cta">About Me</a></li>
+		</ul>
+	</nav>
+
+
+	<!-- Heading -->
+	<div id="cta">
+		<h1>Projects</h1>
+	</div>
+
+	<!-- Main -->
+	<div class="panel-body" style="
+    padding: 100;">
+
+		<!-- Image -->
+		<h2 align="center">DIAMOND </h2>
+		<table align="center" id="cta">
+			<td>
+				<div><img src="images/d1.png" width="100" height="100"></div>
+			</td>
+			<td style="vertical-align: middle;">
+				<div class="content" style="font-size:22">
+					<p>Description : It is Diamond selling website.In this website, I have different module and
+						features.
+						Like I have different type of diamonds and deeply details about it and online purchase.</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Languages : PHP,Html,CSS,JavaScript</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Tools : Visual Studio Code,Wamp Server</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Database : MySQL</p>
+				</div>
+				<ul class="plain">
+					<li><a href="https://github.com/Ap2318/Diamond/tree/master"
+							style="text-decoration:none; color:white;"><i
+								class="fa fa-github">&nbsp;&nbsp;&nbsp;&nbsp;</i>Source Code</a>
+					</li>
+				</ul>
+			</td>
+			<tr>
+		</table>
+		<h2 align="center"> ART-O-MEDIA </h2>
+		<table align="center" id="cta">
+			<td>
+				<div><img src="images/artist.jpg" width="100" height="100"></div>
+			</td>
+			<td style="vertical-align: middle;">
+				<div class="content" style="font-size:22">
+					<p>Description : This is also website called ART-O-MEDIA.It is online artist selling website.we are
+						providing
+						different type of artist for different events like wedding and party.</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Languages : PHP,Html,CSS,JavaScript</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Tools : Visual Studio Code,Wamp Server</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Database : MySQL</p>
+				</div>
+				<ul class="plain">
+					<li><a href="https://github.com/Ap2318/Diamond/tree/master"
+							style="text-decoration:none; color:white;"><i
+								class="fa fa-github">&nbsp;&nbsp;&nbsp;&nbsp;</i>Source Code</a>
+					</li>
+				</ul>
+			</td>
+			<tr>
+		</table>
+		<h2 align="center"> DIAMOND APP </h2>
+		<table align="center" id="cta">
+			<td>
+				<div><img src="images/logo.jpg" width="100" height="100"></div>
+			</td>
+			<td style="vertical-align: middle;">
+				<div class="content" style="font-size:22">
+					<p>Description : It is Diamond selling application.In this application, I have different module and
+						features.
+						Like I have different type of diamonds and deeply details about it and online purchase.</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Languages : JAVA,ANDROID</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Tools : Android Studio Code</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Database : MySQL</p>
+				</div>
+				<ul class="plain">
+					<li><a href="https://github.com/Ap2318/Diamond/tree/master"
+							style="text-decoration:none; color:white;"><i
+								class="fa fa-github">&nbsp;&nbsp;&nbsp;&nbsp;</i>Source Code</a>
+					</li>
+				</ul>
+			</td>
+			<tr>
+		</table>
+		<h2 align="center"> BASIC FILTER </h2>
+		<table align="center" id="cta">
+			<td style="vertical-align: middle;">
+				<div class="content" style="font-size:22">
+					<p>Description : This is the basic filter and list. </p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Languages : ADVANCE JAVA</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Tools : ECLIPSE</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Framework : MVC</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Database : MySQL</p>
+				</div>
+				<ul class="plain">
+					<li><a href="https://github.com/Ap2318/Diamond/tree/master"
+							style="text-decoration:none; color:white;"><i
+								class="fa fa-github">&nbsp;&nbsp;&nbsp;&nbsp;</i>Source Code</a>
+					</li>
+				</ul>
+			</td>
+			<tr>
+		</table>
+		<h2 align="center"> PONG </h2>
+		<table align="center" id="cta">
+			<td style="vertical-align: middle;">
+				<div class="content" style="font-size:22">
+					<p>Description : This is game. It has two paddle and one ball with score board. </p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Languages : ADVANCE C++</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Tools : NETBEANS</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Framework : MVC</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Database : MySQL</p>
+				</div>
+				<ul class="plain">
+					<li><a href="https://github.com/Ap2318/Diamond/tree/master"
+							style="text-decoration:none; color:white;"><i
+								class="fa fa-github">&nbsp;&nbsp;&nbsp;&nbsp;</i>Source Code</a>
+					</li>
+				</ul>
+			</td>
+			<tr>
+		</table>
+		<h2 align="center"> TIC-TOC-TOE </h2>
+		<table align="center" id="cta">
+			<td style="vertical-align: middle;">
+				<div class="content" style="font-size:22">
+					<p>Description : This is game. It has two player and one board with words"X" and "O". </p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Languages : Html, React Native</p>
+				</div>
+				<div class="content" style="font-size:22">
+					<p>Tools : VISUAL STUDIO CODE</p>
+				</div>
+				<ul class="plain">
+					<li><a href="https://github.com/Ap2318/Diamond/tree/master"
+							style="text-decoration:none; color:white;"><i
+								class="fa fa-github">&nbsp;&nbsp;&nbsp;&nbsp;</i>Source Code</a>
+					</li>
+				</ul>
+			</td>
+			<tr>
+		</table>
+	</div>
+	</div>
+
+
+
+
+
+	</div>
+	</div>
+	</div>
+	</div>
+	</section>
+
+	<!-- Footer -->
+	<!-- Footer -->
+	<footer id="footer">
+		<div class="inner">
+			<div class="content">
+				<section>
+					<p><b>Contact</b><br />
+						Name : Arpit Patel<br />
+						Email : arpitp16197@gmail.com<br />
+						Phone No. : +1 438-336-2339</p>
+
+				</section>
+				<section>
+					<h4>Menu</h4>
+					<ul class="alt">
+						<li><a href="index.php#banner">Home</a></li>
+						<li><a href="index.php#projects">Projects</a></li>
+						<li><a href="index.php#technicalskills">Technical Skills</a></li>
+						<li><a href="index.php#contactme">Contact Me</a></li>
+						<li><a href="index.php#cta">About Me</a></li>
+
+					</ul>
+				</section>
+				<section>
+
+					<ul class="plain">
+						<li><a href="https://linkedin.com/in/arpitpatel2323"><i
+									class="icon fa-linkedin">&nbsp;</i>LinkedIn</a>
+						</li>
+						<li><a href="https://github.com/Ap2318"><i class="icon fa-github">&nbsp;</i>Github</a></li>
+					</ul>
+				</section>
+			</div>
+			<div class="copyright">
+				@2021 Arpit Patel
+			</div>
+		</div>
+	</footer>
+
+
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/browser.min.js"></script>
+	<script src="assets/js/breakpoints.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
